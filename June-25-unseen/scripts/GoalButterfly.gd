1da9f35625933576765379cdc7a5a3d859073745
turtletennis <stephenjenkins42@gmail.com>
2025-07-04 21:17:16 +0100

    add light to goal butterfly, make particles more impressive and stop it from being triggered twice

--- June-25-unseen/scripts/GoalButterfly.gd
+++ June-25-unseen/scripts/GoalButterfly.gd
@@ -2,12 +2,15 @@ extends Area2D
 @export var next_scene:PackedScene
 @export var next_scene_timeout:float = 1
 @onready var sfx_player:AudioStreamPlayer = $LevelComplete_AudioStreamPlayer
+var collected = false
 func _on_body_entered(body:Node2D) -> void:
-				if body.is_in_group("player"):
-												GameManager.complete_level()
-												sfx_player.play()
-												var timer = get_tree().create_timer(next_scene_timeout)
-												timer.timeout.connect(_change_scene)
+	if(collected): return
+	collected = true
+	if body.is_in_group("player"):
+									GameManager.complete_level()
+									sfx_player.play()
+									var timer = get_tree().create_timer(next_scene_timeout)
+									timer.timeout.connect(_change_scene)
 												
 func _change_scene():
 		print_debug("load scene") 
